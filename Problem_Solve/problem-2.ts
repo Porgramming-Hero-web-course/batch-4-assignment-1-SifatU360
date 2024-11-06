c38af626e564403efe4bbf47b8cba2b53dfc5e0d
SifatU360 <sifatullah30.info@gmail.com>
2024-11-07 00:03:45 +0600

    Problem-2 solved

---
+++ Problem_Solve/problem-2.ts
@@ -0,0 +1,11 @@
+{
+    //
+    
+    const removeDuplicates =(arr: number[]):number[] =>{
+        return  [...new Set(arr)];
+    }
+
+    // console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
+    
+    //
+}
