2a91a4095c588db4aaab729d2e190238d317edcd
SifatU360 <sifatullah30.info@gmail.com>
2024-11-07 00:25:14 +0600

    Problem-3 solved

---
+++ Problem_Solve/problem-3.ts
@@ -0,0 +1,20 @@
+{
+    //
+
+    const countWordOccurrences = (sentence: string, word: string): number => {
+        const sentenceInLowerCase = sentence.toLowerCase();
+        const wordInLowerCase = word.toLowerCase();
+        const words = sentenceInLowerCase.split(' ').map(word => word.replace(/[^\w]/g, ''));
+        let count = 0;
+        for (let i = 0; i < words.length; i++) {
+            if (words[i] === wordInLowerCase) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // console.log(countWordOccurrences("TypeScript is great. I love TypeScript!", "typescript"));
+
+    //
+}
