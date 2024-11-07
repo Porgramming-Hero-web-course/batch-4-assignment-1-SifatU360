deeb21dea61dd24b8915f7b643ee55da5bb51ec1
SifatU360 <sifatullah30.info@gmail.com>
2024-11-07 17:05:11 +0600

    Problem-3 solved

--- Problem_Solve/problem-3.ts
+++ Problem_Solve/problem-3.ts
@@ -4,7 +4,7 @@
     const countWordOccurrences = (sentence: string, word: string): number => {
         const sentenceInLowerCase = sentence.toLowerCase();
         const wordInLowerCase = word.toLowerCase();
-        const words = sentenceInLowerCase.split(' ').map(word => word.replace(/[^\w]/g, ''));
+        const words = sentenceInLowerCase.split(' ');
         let count = 0;
         for (let i = 0; i < words.length; i++) {
             if (words[i] === wordInLowerCase) {
